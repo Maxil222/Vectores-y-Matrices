fd14cf0275fedf167e612933ed2dbbe8fe2cbea7
Maxi Masson <dammysl222@gmail.com>
2022-08-26 11:35:04 -0300

    Ejercicio4

---
+++ Ejercicio4.py
@@ -0,0 +1,11 @@
+print("Ingrese dimension de la matriz, maximo 100")
+print("Dimension S")
+S = []
+print("Dimension R")
+R = []
+for i in range (S):
+    for j in range (R):
+        A = int(input("Datos de la Matriz A {0}".format(i,j)))
+        B = int(input("Datos de la Matriz B {0}".format(i,j)))
+        C = int(input("Datos de la Matriz C {0}".format(i,j)))
+        C.append(A + C) 
